b8ea4d15edd2decd2d42796e60ce390d9874daa6
Victor Kalyanapu <vkalyanapu@brightree.com>
2022-07-07 15:05:12 -0400

    initial commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleDrawingApp;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace SampleDrawingApp;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void SignaturePadView_DrawingLineCompleted(object sender, CommunityToolkit.Maui.Core.DrawingLineCompletedEventArgs e)
+    {
+        try
+        {
+            string signaturesDirectoryPath = System.IO.Path.Combine(FileSystem.Current.AppDataDirectory, "Signatures");
+
+            if(!Directory.Exists(signaturesDirectoryPath))
+                Directory.CreateDirectory(signaturesDirectoryPath);
+
+            string filename = System.IO.Path.Combine(signaturesDirectoryPath, "Signature.png");
+            using (FileStream outStream = new FileStream(filename, FileMode.Create))
+            {
+                using (var imageStream = await SignaturePadView.GetImageStream(SignaturePadView.Width, SignaturePadView.Height))
+                {
+                    imageStream.CopyTo(outStream);
+                }
+            }
+           
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Failed to save. "+ ex);
+            //need to show a toast
+        }
+    }
+}
+
